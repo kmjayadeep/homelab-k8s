e68e997fd60a4dac86d547a672e88d745600c505
Jayadeep KM <kmjayadeep@gmail.com>
2023-02-12 14:22:26 +0100

    fix alertmanager secret
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- clusters/andromeda/infra/monitoring/mksecret-alertmanager.sh
+++ clusters/andromeda/infra/monitoring/mksecret-alertmanager.sh
@@ -2,5 +2,5 @@
 
 # mksecret-alertmanager.sh: Create alertmanager secret
 
-kubectl create secret generic alertmanager-config --from-file=alertmanager.yml=config-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-decrypted.yaml
+kubectl create secret generic alertmanager-config --from-file=alertmanager.yaml=config-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-decrypted.yaml
 kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-decrypted.yaml > alermanager-config-sealed.yaml
