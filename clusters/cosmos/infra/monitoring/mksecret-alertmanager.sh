9ad14e7661ece27f8215e59567efb844f6639ef3
Jayadeep KM <kmjayadeep@gmail.com>
2023-07-22 21:40:06 +0200

    fix monitoring secrets
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- clusters/cosmos/infra/monitoring/mksecret-alertmanager.sh
+++ clusters/cosmos/infra/monitoring/mksecret-alertmanager.sh
@@ -2,5 +2,5 @@
 
 # mksecret-alertmanager.sh: Create alertmanager secret
 
-kubectl create secret generic alertmanager-config --from-file=alertmanager.yaml=config-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-decrypted.yaml
-kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-decrypted.yaml > alermanager-config-sealed.yaml
+kubectl create secret generic alertmanager-config -n monitoring --from-file=alertmanager.yaml=config/alertmanager-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-config-decrypted.yaml
+kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-config-decrypted.yaml > alermanager-config-sealed.yaml
