1688dd9b13ed1f685d4f1d09df250ee2fe26bb0a
Jayadeep KM <kmjayadeep@gmail.com>
2023-07-25 21:53:25 +0200

    reinitialize milkyway
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- clusters/andromeda/infra/monitoring/mksecret-alertmanager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# mksecret-alertmanager.sh: Create alertmanager secret
-
-kubectl create secret generic alertmanager-config --from-file=alertmanager.yaml=config-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-decrypted.yaml
-kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-decrypted.yaml > alermanager-config-sealed.yaml
