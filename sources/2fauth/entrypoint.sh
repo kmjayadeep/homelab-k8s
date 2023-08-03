4b1666ff4372912131b845cab94977db0979d624
Jayadeep KM <kmjayadeep@gmail.com>
2023-08-03 21:36:44 +0200

    add pvc
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- sources/2fauth/entrypoint.sh
+++ sources/2fauth/entrypoint.sh
@@ -10,12 +10,12 @@ php-fpm81 -v | head -n 1
 nginx -v
 
 # Inject storage in /2fauth and use it with a symlink
-if [ ! -d /2fauth/storage ]; then
-  mv /srv/storage /2fauth/storage
-else
-  rm -r /srv/storage
-fi
-ln -s /2fauth/storage /srv/storage
+# if [ ! -d /2fauth/storage ]; then
+#   mv /srv/storage /2fauth/storage
+# else
+#   rm -r /srv/storage
+# fi
+# ln -s /2fauth/storage /srv/storage
 
 php artisan migrate
 php artisan storage:link --quiet
