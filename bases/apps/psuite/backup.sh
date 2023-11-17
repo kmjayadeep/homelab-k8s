56588548c8abf59d85336ce769ec5f8b5995bde8
Jayadeep KM <kmjayadeep@gmail.com>
2023-11-17 01:32:44 +0100

    psuite: enable backup job and init
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- bases/apps/psuite/backup.sh
+++ bases/apps/psuite/backup.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-while true;
-do
-  echo "backing up";
-  restic --verbose backup /data /config
-  echo "backup successful"
-  echo "cleaning up old snapshots"
-  restic forget --keep-monthly 6 --keep-last 7 --keep-daily 10 --prune
-  echo "current snapshots:"
-  restic snapshots
-  sleep 3600
-done
+echo "backing up";
+restic --verbose backup /data /config
+echo "backup successful"
+echo "cleaning up old snapshots"
+restic forget --keep-monthly 6 --keep-last 7 --keep-daily 10 --prune
+echo "current snapshots:"
+restic snapshots
