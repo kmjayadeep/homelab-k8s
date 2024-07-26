c1968182768ceb11422970412a357582c72c38f3
Jayadeep KM <kmjayadeep@gmail.com>
2024-07-26 17:23:11 +0200

    move monitoring to titania
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ clusters/titania/infra/monitoring/mksecret-alertmanager.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# mksecret-alertmanager.sh: Create alertmanager secret
+
+kubectl create secret generic alertmanager-config -n monitoring --from-file=alertmanager.yaml=config/alertmanager-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-config-decrypted.yaml
+kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-config-decrypted.yaml > alermanager-config-sealed.yaml
