85251b6ff1b68da11209879c49403938b133ed4a
Jayadeep KM <kmjayadeep@gmail.com>
2025-04-14 20:10:36 +0200

    cleanup old cluster yaml
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- clusters/cosmos/bootstrap-disabled/README.md
+++
@@ -1,3 +0,0 @@
-# Readme
-
-This folder contains Apps which are disabled at the moment and removed from bootstrap folder.
--- clusters/cosmos/infra/monitoring/mksecret-alertmanager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# mksecret-alertmanager.sh: Create alertmanager secret
-
-kubectl create secret generic alertmanager-config -n monitoring --from-file=alertmanager.yaml=config/alertmanager-decrypted.yaml --dry-run=client -oyaml > ./alertmanager-config-decrypted.yaml
-kubeseal --format=yaml --cert ../../../../kubeseal/pub-sealed-secrets.pem < alertmanager-config-decrypted.yaml > alermanager-config-sealed.yaml
