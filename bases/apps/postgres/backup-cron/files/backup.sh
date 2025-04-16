1104f883350f1a9940e0ee1478ac1b9c9f471c11
Jayadeep KM <kmjayadeep@gmail.com>
2025-04-16 05:38:10 +0200

    simplify shoppinglist
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- bases/apps/postgres/backup-cron/files/backup.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-ls /backup
-
-restic backup -H automation-job /backup
-
-restic forget --keep-monthly 12 --keep-last 10 --keep-daily 10 --prune
-
-restic snapshots
--- bases/apps/postgres/backup-cron/files/pg_dump.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-for DB_NAME in $(echo $DATABASES | tr ',' ' '); do
-  pg_dump -c -d "$DB_NAME" -F tar -f "/backup/${DB_NAME}_backup.sql"
-done
